c139169115f2754b7609b367e65bf0848072829b
DuncanGermain <TK17Studios@gmail.com>
2015-05-05 11:07:06 -0700

    Adding working Angular skeleton (to be fused with HTML frame).

---
+++ Angularized/Routes/js/portfolio.js
@@ -0,0 +1,77 @@
+var portfolioApp = angular.module('portfolioApp', ['ngRoute']);
+
+portfolioApp.controller('displayController', function($scope) {
+  $scope.message = "I'm the display controller, wired to the 'main' html tag.";
+});
+
+
+portfolioApp.config(function($routeProvider) {
+  $routeProvider
+    .when('/main', {
+      templateUrl: 'partials/main.html',
+      controller:  'MainController'
+    })
+    .when('/about', {
+      templateUrl: 'partials/about.html',
+      controller:  'AboutController'
+    })
+    .when('/apparel', {
+      templateUrl: 'partials/apparel.html',
+      controller:  'ApparelController'
+    })
+    .when('/art', {
+      templateUrl: 'partials/art.html',
+      controller:  'ArtController'
+    })
+    .when('/code', {
+      templateUrl: 'partials/code.html',
+      controller:  'CodeController'
+    })
+    .when('/design', {
+      templateUrl: 'partials/design.html',
+      controller:  'DesignController'
+    })
+    .when('/video', {
+      templateUrl: 'partials/video.html',
+      controller:  'VideoController'
+    })
+    .when('/writing', {
+      templateUrl: 'partials/writing.html',
+      controller:  'WritingController'
+    })
+    .otherwise({
+      redirectTo: '/main'
+    });
+});
+
+    portfolioApp.controller('MainController', function($scope) {
+        $scope.message = "I'll be a handful of cool icons.";
+    });
+
+    portfolioApp.controller('AboutController', function($scope) {
+        $scope.message = "I'll be a résumé and articles about Duncan.";
+    });
+
+    portfolioApp.controller('ApparelController', function($scope) {
+        $scope.message = "I'll be a list of apparel projects.";
+    });
+
+    portfolioApp.controller('ArtController', function($scope) {
+        $scope.message = "I'll be a list of art projects.";
+    });
+
+    portfolioApp.controller('CodeController', function($scope) {
+        $scope.message = "I'll be a list of code projects.";
+    });
+
+    portfolioApp.controller('DesignController', function($scope) {
+        $scope.message = "I'll be a list of design projects.";
+    });
+
+    portfolioApp.controller('VideoController', function($scope) {
+        $scope.message = "I'll be a list of video projects.";
+    });
+
+    portfolioApp.controller('WritingController', function($scope) {
+        $scope.message = "I'll be a list of writing projects.";
+    });
