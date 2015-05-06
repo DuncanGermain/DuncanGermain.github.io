1bfb2c0ecfb053955c8d4114283997d7091e0c38
DuncanGermain <TK17Studios@gmail.com>
2015-05-06 12:35:33 -0700

    Adding bottom layer (projects pages) through population.

--- Angularized/Routes/js/portfolio.js
+++ Angularized/Routes/js/portfolio.js
@@ -9,6 +9,9 @@ portfolioApp.config(function($routeProvider) {
     }).when('/:field', {
       templateUrl: 'partials/field.html',
       controller:  'FieldController'
+    }).when('/:field/:project', {
+      templateUrl: 'partials/project.html',
+      controller:  'ProjectController'
     }).otherwise({
       redirectTo: '/main'
     });
@@ -18,7 +21,14 @@ portfolioApp.controller('MainController', function($scope) {
 });
 
 portfolioApp.controller('FieldController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+    $scope.field = $routeParams.field;
     $http.get('json/' + $routeParams.field + '.json').success(function(data) {
+      $scope.snippets = data;
+    });
+}]);
+
+portfolioApp.controller('ProjectController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+    $http.get('json/' + $routeParams.project + '.json').success(function(data) {
       $scope.snippets = data;
     });
 }]);
